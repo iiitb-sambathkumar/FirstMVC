79d29b6bcf9f2d5c6fe71278fd112f14962573fd
iiitb-sambathkumar <sambathkumarnat.dsd18@iiitb.net>
2020-12-12 10:40:21 +0530

    Initial commit

---
+++ src/main/java/Upgrad/FirstMVC/FirstMvcApplication.java
@@ -0,0 +1,13 @@
+package Upgrad.FirstMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMvcApplication.class, args);
+	}
+
+}
